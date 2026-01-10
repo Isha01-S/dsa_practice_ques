7ccf4c15e48fb329c2ec8c19573a32d1e2e6171a
Isha Sriwastav <ishamca01@gmail.com>
2026-01-10 23:42:25 +0530

    Time: 7 ms (17.74%), Space: 12.5 MB (66.34%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.py
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.py
@@ -4,25 +4,49 @@ class Solution(object):
         :type s: str
         :rtype: str
         """
-        s=s.strip()
-        array = []
-        st=""
-        result=""
+        # s=s.strip()
+        # array = []
+        # st=""
+        # result=""
+        # for ch in s:
+        #     if ch == " ":
+        #         if st=="":
+        #             continue
+        #         array.append(st)
+        #         st = ""
+        #     else:
+        #         st+=ch
+        # array.append(st)
+        # for i in range(len(array)-1,-1,-1):
+        #     result+=array[i]
+        #     if i==0:
+        #         break
+        #     result+=" "
+        # return result
+
+        s = s.strip()          # remove leading/trailing spaces
+        words = []
+        current_word = ""
+
+        # collect words
         for ch in s:
             if ch == " ":
-                if st=="":
-                    continue
-                array.append(st)
-                st = ""
+                if current_word:
+                    words.append(current_word)
+                    current_word = ""
             else:
-                st+=ch
-        array.append(st)
-        for i in range(len(array)-1,-1,-1):
-            result+=array[i]
-            if i==0:
-                break
-            result+=" "
-        return result
+                current_word += ch
 
-            
+        # append last word if exists
+        if current_word:
+            words.append(current_word)
+
+        # build reversed result
+        result = ""
+        for i in range(len(words) - 1, -1, -1):
+            result += words[i]
+            if i > 0:
+                result += " "
+
+        return result            
         
